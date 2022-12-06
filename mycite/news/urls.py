2279c80a65364479e12e9ab926ac0c0291818939
bog <bog@odoo.com>
2022-12-06 12:53:41 +0300

    added ckeditor

--- mycite/news/urls.py
+++ mycite/news/urls.py
@@ -3,9 +3,13 @@ from .views import *
 
 
 urlpatterns = [
+    path('register/', register, name='register'),
+    path('login/', user_login, name='login'),
+    path('logout/', user_logout, name='logout'),
     path('', HomeNews.as_view(), name='home'),
     path('category/<int:category_id>/', CategoryList.as_view(), name='category'),
     path('news/<int:pk>/', ViewNews.as_view(), name='news'),
     path('news/add-news/', CreateNews.as_view(), name='add_news'),
+
 ]
 
--- mycite/news/views.py
+++ mycite/news/views.py
@@ -1,8 +1,44 @@
+from django.contrib.auth import login, logout
+from django.shortcuts import render, redirect
 from django.views.generic import ListView, DetailView, CreateView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import News, Category
-from .forms import NewsForm
+from .forms import NewsForm, UserRegisterForm, UserLoginForm
 from .utils import MyMixin
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
+
+
+def register(request):
+    if request.method == "POST":
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            messages.success(request, 'Тебе удалось!')
+            return redirect('home')
+        else:
+            messages.error(request, 'БЕДА')
+    else:
+        form = UserRegisterForm()
+    return render(request, 'news/register.html', {'form': form})
+
+
+def user_login(request):
+    if request.method == 'POST':
+        form = UserLoginForm(data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect('home')
+    else:
+        form = UserLoginForm()
+    return render(request, 'news/login.html', {'form': form})
+
+
+def user_logout(request):
+    logout(request)
+    return redirect('login')
 
 
 class HomeNews(MyMixin, ListView):
